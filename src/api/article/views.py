86ea604a9e8d86232777f449c1b8c06a05ecea2c
vanh <caovietanhhd@gmail.com>
2023-07-28 15:59:01 +0700

    article-api

--- src/api/article/views.py
+++ src/api/article/views.py
@@ -2,22 +2,47 @@ from flask import Blueprint
 from src.api.urls import Endpoint
 from src.api import HttpMethod
 from src.common.utils.alchemy import execute_sql
-from sqlalchemy.sql import text
 blueprint = Blueprint("article",__name__)
 
 @blueprint.route(Endpoint.ARTICLE, methods =[HttpMethod.GET])
 def get_articles():
-    state = "SELECT * FROM conduit.article" 
+    state = """SELECT
+	slug,
+	title,
+	description,
+	body,
+	created_at,
+	updated_at,
+	username,
+	bio,
+	image 
+FROM
+conduit.article AS article
+LEFT JOIN conduit.author ON article.author_name = conduit.author.username""" 
+    
     result = execute_sql(state)
-    author = {
-        "username": "jake",
-        "bio": "I work at statefarm",
-        "image": "https://i.stack.imgur.com/xHWG8.jpg",
-        "following": False
-    }
-    author['id'] = 1
-    for a in result : 
-        a["author"]= author
+    author = {}
+    for a in result :
+        b = a["updated_at"]
+        c = a["created_at"]
+        del a["created_at"] 
+        del a["updated_at"]
+        a['updatedAt'] = b 
+        a['createdAt'] = c 
+        a['tagList'] = ["dragons", "training"]
+        a["favorited"] = False
+        a["favoritesCount"] = 0
+        username = a["username"]
+        bio = a["bio"] 
+        image = a["image"]
+        author["username"] = username 
+        author["bio"] = bio 
+        author["image"] = image
+        del a["username"] 
+        del a["bio"]
+        del a["image"]
+        author["following"] = False  
+        a["author"] = author  
     dict_return = {"articles":result,
                    "articlesCount" :len(result),
                 }
@@ -26,9 +51,53 @@ def get_articles():
 
 
 
-# @blueprint.route(Endpoint.ARTICLE1, methods =[HttpMethod.GET])
-# def get_single_article(slug):
-#     return slug 
+@blueprint.route(Endpoint.ARTICLE1, methods =[HttpMethod.GET])
+def get_single_article(slug):
+    state = f"""SELECT
+	slug,
+	title,
+	description,
+	body,
+	created_at,
+	updated_at,
+	username,
+	bio,
+	image 
+FROM
+	conduit.article AS article
+	LEFT JOIN conduit.author ON article.author_name = conduit.author.username 
+WHERE
+	article.slug = '{slug}' """
+    result = execute_sql(state)
+    if len(result) == 0 :
+        return {} 
+    author = {}
+    result = result[0] 
+    b = result["updated_at"]
+    c = result["created_at"]
+    d = result
+    del result["created_at"] 
+    del result["updated_at"]
+    result['updatedAt'] = b 
+    result['createdAt'] = c 
+    result['tagList'] = ["dragons", "training"]
+    result["favorited"] = False
+    result["favoritesCount"] = 0
+    username = result["username"]
+    bio = result["bio"] 
+    image = result["image"]
+    author["username"] = username 
+    author["bio"] = bio 
+    author["image"] = image
+    del result["username"] 
+    del result["bio"]
+    del result["image"]
+    author["following"] = False  
+    result["author"] = author  
+    dict_return = {"article":result,
+            }
+    
+    return dict_return 
 
 # @blueprint.route(Endpoint.ARTICLE, methods =[HttpMethod.POST])
 # def create_article():
---
+++ src/sql/conduit.sql
@@ -0,0 +1,59 @@
+DROP TABLE IF EXISTS conduit.favorited ;
+DROP TABLE IF EXISTS condui.followed ;
+DROP TABLE IF EXISTS conduit.tag ;
+DROP TABLE IF EXISTS conduit.article ; 
+DROP TABLE IF EXISTS conduit.author ;
+
+CREATE TABLE conduit.author (
+    username TEXT UNIQUE PRIMARY KEY , 
+    password  TEXT NOT NULL , 
+    image TEXT ,  
+    bio TEXT  
+);
+CREATE TABLE conduit.article (
+    slug TEXT PRIMARY KEY ,
+    title TEXT NOT NULL ,
+    description TEXT NOT NULL , 
+    body TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE ,
+    updated_at TIMESTAMP WITH TIME ZONE ,
+    author_name TEXT,
+    CONSTRAINT fk_author
+    FOREIGN KEY(author_name) 
+    REFERENCES conduit.author(username)
+);
+
+
+CREATE TABLE conduit.tag (
+    id SERIAL PRIMARY KEY ,
+    tag_name TEXT ,
+    article_slug TEXT ,
+    CONSTRAINT fk_article 
+    FOREIGN KEY(article_slug)
+    REFERENCES conduit.article(slug)
+);
+CREATE TABLE conduit.favorited (
+    article_slug TEXT ,
+    username TEXT , 
+    PRIMARY KEY (article_slug,username),
+    CONSTRAINT fk_user 
+    FOREIGN KEY (username)
+    REFERENCES conduit.author(username),
+    CONSTRAINT fk_article
+    FOREIGN KEY (article_slug) 
+    REFERENCES conduit.article(slug)
+);
+CREATE TABLE conduit.followed (
+    author_username TEXT ,
+    user_username TEXT ,
+    PRIMARY KEY (author_username,user_username) ,
+    CONSTRAINT fk_author 
+    FOREIGN KEY (author_username)
+    REFERENCES conduit.author(username),
+    CONSTRAINT fk_user_follow
+    FOREIGN KEY (user_username) 
+    REFERENCES conduit.author(username)
+);
+
+
+
